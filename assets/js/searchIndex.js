d28a4f95af91d870028a242ee912219d3bc7aa91
Gary Ewan Park <gep13@gep13.co.uk>
2017-02-01 21:31:48 +0000

    AppVeyor Publish: d50826875f395543818bd490659c4ad6ce89ab60 Merge branch 'release/0.2.0' into develop
    * release/0.2.0:
      (doc) Added release blog post

--- assets/js/searchIndex.js
+++ assets/js/searchIndex.js
@@ -34,14 +34,6 @@ var searchModule = function() {
 
     a({
         id:0,
-        title:"SendGridSettings",
-        content:"SendGridSettings",
-        description:'',
-        tags:''
-    });
-
-    a({
-        id:1,
         title:"SendGridAliases",
         content:"SendGridAliases",
         description:'',
@@ -49,25 +41,27 @@ var searchModule = function() {
     });
 
     a({
-        id:2,
-        title:"SendGridResult",
-        content:"SendGridResult",
-        description:'',
-        tags:''
-    });
-
-    a({
-        id:3,
+        id:1,
         title:"SendGridProvider",
         content:"SendGridProvider",
         description:'',
         tags:''
     });
 
-    y({
-        url:'/Cake.SendGrid/Cake.SendGrid/api/Cake.SendGrid.Email/SendGridSettings',
+    a({
+        id:2,
         title:"SendGridSettings",
-        description:""
+        content:"SendGridSettings",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:3,
+        title:"SendGridResult",
+        content:"SendGridResult",
+        description:'',
+        tags:''
     });
 
     y({
@@ -77,14 +71,20 @@ var searchModule = function() {
     });
 
     y({
-        url:'/Cake.SendGrid/Cake.SendGrid/api/Cake.SendGrid/SendGridResult',
-        title:"SendGridResult",
+        url:'/Cake.SendGrid/Cake.SendGrid/api/Cake.SendGrid/SendGridProvider',
+        title:"SendGridProvider",
         description:""
     });
 
     y({
-        url:'/Cake.SendGrid/Cake.SendGrid/api/Cake.SendGrid/SendGridProvider',
-        title:"SendGridProvider",
+        url:'/Cake.SendGrid/Cake.SendGrid/api/Cake.SendGrid.Email/SendGridSettings',
+        title:"SendGridSettings",
+        description:""
+    });
+
+    y({
+        url:'/Cake.SendGrid/Cake.SendGrid/api/Cake.SendGrid/SendGridResult',
+        title:"SendGridResult",
         description:""
     });
 
