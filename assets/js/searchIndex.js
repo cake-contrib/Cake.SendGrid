19ba1650f0233b267796ca0a23cd6e57331cfe1f
Gary Ewan Park <gep13@gep13.co.uk>
2017-02-01 21:20:00 +0000

    AppVeyor Publish: 65b02637fa2e6a4219ec852ea63324d17a386296 (GH-12) Added Wyam Docs

---
+++ assets/js/searchIndex.js
@@ -0,0 +1,98 @@
+
+var camelCaseTokenizer = function (obj) {
+    var previous = '';
+    return obj.toString().trim().split(/[\s\-]+|(?=[A-Z])/).reduce(function(acc, cur) {
+        var current = cur.toLowerCase();
+        if(acc.length === 0) {
+            previous = current;
+            return acc.concat(current);
+        }
+        previous = previous.concat(current);
+        return acc.concat([current, previous]);
+    }, []);
+}
+lunr.tokenizer.registerFunction(camelCaseTokenizer, 'camelCaseTokenizer')
+var searchModule = function() {
+    var idMap = [];
+    function y(e) { 
+        idMap.push(e); 
+    }
+    var idx = lunr(function() {
+        this.field('title', { boost: 10 });
+        this.field('content');
+        this.field('description', { boost: 5 });
+        this.field('tags', { boost: 50 });
+        this.ref('id');
+        this.tokenizer(camelCaseTokenizer);
+
+        this.pipeline.remove(lunr.stopWordFilter);
+        this.pipeline.remove(lunr.stemmer);
+    });
+    function a(e) { 
+        idx.add(e); 
+    }
+
+    a({
+        id:0,
+        title:"SendGridAliases",
+        content:"SendGridAliases",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:1,
+        title:"SendGridResult",
+        content:"SendGridResult",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:2,
+        title:"SendGridProvider",
+        content:"SendGridProvider",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:3,
+        title:"SendGridSettings",
+        content:"SendGridSettings",
+        description:'',
+        tags:''
+    });
+
+    y({
+        url:'/Cake.SendGrid/Cake.SendGrid/api/Cake.SendGrid/SendGridAliases',
+        title:"SendGridAliases",
+        description:""
+    });
+
+    y({
+        url:'/Cake.SendGrid/Cake.SendGrid/api/Cake.SendGrid/SendGridResult',
+        title:"SendGridResult",
+        description:""
+    });
+
+    y({
+        url:'/Cake.SendGrid/Cake.SendGrid/api/Cake.SendGrid/SendGridProvider',
+        title:"SendGridProvider",
+        description:""
+    });
+
+    y({
+        url:'/Cake.SendGrid/Cake.SendGrid/api/Cake.SendGrid.Email/SendGridSettings',
+        title:"SendGridSettings",
+        description:""
+    });
+
+    return {
+        search: function(q) {
+            return idx.search(q).map(function(i) {
+                return idMap[i.ref];
+            });
+        }
+    };
+}();
