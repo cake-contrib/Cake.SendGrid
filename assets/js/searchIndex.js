710591566d6f3f0e189a95d29fdfb8efe7572899
Jeremie Desautels <desautelsj@hotmail.com>
2017-10-20 10:33:53 -0700

    AppVeyor Publish: 35c5d2afa0c7eca391a019b85327d3c2a8496f65 Merge branch 'release/0.5.0'

--- assets/js/searchIndex.js
+++ assets/js/searchIndex.js
@@ -34,8 +34,8 @@ var searchModule = function() {
 
     a({
         id:0,
-        title:"SendGridAliases",
-        content:"SendGridAliases",
+        title:"SendGridResult",
+        content:"SendGridResult",
         description:'',
         tags:''
     });
@@ -50,23 +50,23 @@ var searchModule = function() {
 
     a({
         id:2,
-        title:"SendGridResult",
-        content:"SendGridResult",
-        description:'',
-        tags:''
-    });
-
-    a({
-        id:3,
         title:"SendGridProvider",
         content:"SendGridProvider",
         description:'',
         tags:''
     });
 
-    y({
-        url:'/Cake.SendGrid/api/Cake.SendGrid/SendGridAliases',
+    a({
+        id:3,
         title:"SendGridAliases",
+        content:"SendGridAliases",
+        description:'',
+        tags:''
+    });
+
+    y({
+        url:'/Cake.SendGrid/api/Cake.SendGrid/SendGridResult',
+        title:"SendGridResult",
         description:""
     });
 
@@ -77,14 +77,14 @@ var searchModule = function() {
     });
 
     y({
-        url:'/Cake.SendGrid/api/Cake.SendGrid/SendGridResult',
-        title:"SendGridResult",
+        url:'/Cake.SendGrid/api/Cake.SendGrid/SendGridProvider',
+        title:"SendGridProvider",
         description:""
     });
 
     y({
-        url:'/Cake.SendGrid/api/Cake.SendGrid/SendGridProvider',
-        title:"SendGridProvider",
+        url:'/Cake.SendGrid/api/Cake.SendGrid/SendGridAliases',
+        title:"SendGridAliases",
         description:""
     });
 
