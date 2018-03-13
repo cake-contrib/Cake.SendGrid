c29e201f2936e9cd76af0e986918da66afa52208
Jeremie Desautels <desautelsj@hotmail.com>
2018-03-13 12:03:55 -0700

    AppVeyor Publish: 9413742067f8e2c2c30598b2a6f4232cd1c1dc87 Merge branch 'release/0.6.0'

--- assets/js/searchIndex.js
+++ assets/js/searchIndex.js
@@ -42,14 +42,6 @@ var searchModule = function() {
 
     a({
         id:1,
-        title:"SendGridAliases",
-        content:"SendGridAliases",
-        description:'',
-        tags:''
-    });
-
-    a({
-        id:2,
         title:"SendGridSettings",
         content:"SendGridSettings",
         description:'',
@@ -57,9 +49,17 @@ var searchModule = function() {
     });
 
     a({
-        id:3,
+        id:2,
         title:"SendGridResult",
         content:"SendGridResult",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:3,
+        title:"SendGridAliases",
+        content:"SendGridAliases",
         description:'',
         tags:''
     });
@@ -67,12 +67,6 @@ var searchModule = function() {
     y({
         url:'/Cake.SendGrid/api/Cake.SendGrid/SendGridProvider',
         title:"SendGridProvider",
-        description:""
-    });
-
-    y({
-        url:'/Cake.SendGrid/api/Cake.SendGrid/SendGridAliases',
-        title:"SendGridAliases",
         description:""
     });
 
@@ -85,6 +79,12 @@ var searchModule = function() {
     y({
         url:'/Cake.SendGrid/api/Cake.SendGrid/SendGridResult',
         title:"SendGridResult",
+        description:""
+    });
+
+    y({
+        url:'/Cake.SendGrid/api/Cake.SendGrid/SendGridAliases',
+        title:"SendGridAliases",
         description:""
     });
 
