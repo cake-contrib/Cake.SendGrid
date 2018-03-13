379d87e75e7402f21445e6a92239dff96fffa4c8
Jeremie Desautels <desautelsj@hotmail.com>
2018-03-13 11:54:35 -0700

    AppVeyor Publish: 069570d961a3e30e0319876d1caa5b859ef5f28c Add blog post about the new release

--- assets/js/searchIndex.js
+++ assets/js/searchIndex.js
@@ -34,22 +34,6 @@ var searchModule = function() {
 
     a({
         id:0,
-        title:"SendGridResult",
-        content:"SendGridResult",
-        description:'',
-        tags:''
-    });
-
-    a({
-        id:1,
-        title:"SendGridSettings",
-        content:"SendGridSettings",
-        description:'',
-        tags:''
-    });
-
-    a({
-        id:2,
         title:"SendGridProvider",
         content:"SendGridProvider",
         description:'',
@@ -57,23 +41,27 @@ var searchModule = function() {
     });
 
     a({
-        id:3,
+        id:1,
         title:"SendGridAliases",
         content:"SendGridAliases",
         description:'',
         tags:''
     });
 
-    y({
-        url:'/Cake.SendGrid/api/Cake.SendGrid/SendGridResult',
-        title:"SendGridResult",
-        description:""
+    a({
+        id:2,
+        title:"SendGridSettings",
+        content:"SendGridSettings",
+        description:'',
+        tags:''
     });
 
-    y({
-        url:'/Cake.SendGrid/api/Cake.SendGrid.Email/SendGridSettings',
-        title:"SendGridSettings",
-        description:""
+    a({
+        id:3,
+        title:"SendGridResult",
+        content:"SendGridResult",
+        description:'',
+        tags:''
     });
 
     y({
@@ -85,6 +73,18 @@ var searchModule = function() {
     y({
         url:'/Cake.SendGrid/api/Cake.SendGrid/SendGridAliases',
         title:"SendGridAliases",
+        description:""
+    });
+
+    y({
+        url:'/Cake.SendGrid/api/Cake.SendGrid.Email/SendGridSettings',
+        title:"SendGridSettings",
+        description:""
+    });
+
+    y({
+        url:'/Cake.SendGrid/api/Cake.SendGrid/SendGridResult',
+        title:"SendGridResult",
         description:""
     });
 
